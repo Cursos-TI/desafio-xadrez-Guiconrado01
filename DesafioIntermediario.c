350d9616be6791f42a592ec50e0e8e854da275ab
Guilherme conrado <guiconrado2015@hotmail.com>
2025-08-01 17:08:25 +0000

    DesafioINTERMEDIARIO

--- DesafioIntermediario.c
+++ DesafioIntermediario.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
+
 int main() {
-  int i = 0;
-  int x = 0;
-  int z = 0;
-  int j = 0;
+    int i = 0;
+    int x = 0;
+    int z = 0;
+    int j = 0;
 
-  for( i = 0; i < 5; i++)
-  {
-    printf("Movimento Torre para Direita\n");
-  }
-    do{
-      printf("Movimento Bispo para Cima,Esquerda\n");
-      x++;
-    }while( x < 5);
-
-    while(z < 8)
-    {
-      printf("Movimento Rainha para esquerda\n", z);
-      z++;
+    for(i = 1; i <= 5; i++) {
+        printf("Movimento %d: Cavalo para Direita\n", i);
     }
 
-    for(j = 0; j < 2; j++){
-      printf("Movimento Cavalo para Cima\n");
+    do {
+        printf("Movimento %d: Bispo para Cima, Esquerda\n", x + 1);
+        x++;
+    } while(x < 5);
+
+    while(z < 8) {
+        printf("Movimento %d: Rainha para Esquerda\n", z + 1);
+        z++;
     }
-    printf("Movimento Cavalo para Esquerda\n");
 
-        
-      
+    for(j = 1; j <= 2; j++) {
+        printf("Movimento %d: Cavalo para Cima\n", j);
+    }
 
-    
+    printf("Movimento Final: Cavalo para Esquerda\n");
 
-
-
-
-  
-  return 0;
+    return 0;
 }
